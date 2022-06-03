669766f8236d6284709577201b9005dc216c6dcd
Reagan Bodily <reaganbodily@gmail.com>
2022-06-02 18:54:22 -0600

    connected to MongoDB Atlas for Heroku Deployment

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
 
+const URI = "mongodb+srv://reaganbodily:SwVg*4a@4Pkaeak@cluster0.rai57wf.mongodb.net/budgettracker?retryWrites=true&w=majority"
+
 const PORT = process.env.PORT || 3001;
 const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/budget";
 
@@ -16,7 +18,7 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(MONGODB_URI, {
+mongoose.connect(URI, {
   useNewUrlParser: true,
   useFindAndModify: false
 });
